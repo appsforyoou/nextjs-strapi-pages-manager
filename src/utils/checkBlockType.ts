76195c84b3766fa2a3866c24eafd9e7a2d150395
Andrei <andreigames9@gmail.com>
2023-07-30 22:14:38 +0200

    isHero block check

--- src/utils/checkBlockType.ts
+++ src/utils/checkBlockType.ts
@@ -1,7 +1,11 @@
-import {BlockInterface, IFooterLinks} from "../blocksInterfaces";
+import {BlockInterface, IFooterLinks, IHero} from "../blocksInterfaces";
 
 export default {
     isFooterLinks: (block: BlockInterface): block is IFooterLinks => {
         return block.__component === 'footer-blocks.links';
+    },
+
+    isHero: (block: BlockInterface): block is IHero => {
+        return block.__component === 'blocks.hero';
     }
 }
