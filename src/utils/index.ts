2ed65d4ded5c57d3bac17d59c517b6ba9922c7d7
Andrei <andreigames9@gmail.com>
2023-07-31 23:01:47 +0200

    fixed getStrapiMedia return string

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,10 +14,8 @@ export function getStrapiMedia(url: string) {
     if (url == null) {
         return null;
     }
-    if (url.startsWith("http") || url.startsWith("//")) {
-        return url;
-    }
-    return `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"}${url}`;
+
+    return `${process.env.NEXT_PUBLIC_STRAPI_HOST || "http://localhost:1337"}${url}`;
 }
 
 // handle the redirection to the homepage if the page we are browsinng doesn't exists
