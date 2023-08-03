fa801a2cc06db81a21ed56e9451d82c1ad06c67a
Andrei <andreigames9@gmail.com>
2023-08-03 23:37:27 +0200

    notices completed

--- src/helpers/getNotices.ts
+++ src/helpers/getNotices.ts
@@ -1,19 +1,34 @@
 import parsedStrapiUrl from '../utils/parseStrapiUrl';
+import {buildStrapiQuery} from "../utils";
 
-export async function getLegalNotice() {
-    const res = await fetch(parsedStrapiUrl + 'legal-notice', {
+export async function getLegalNotice(lang: string) {
+    const query = buildStrapiQuery({
+        locale: lang,
+    })
+    const res = await fetch(parsedStrapiUrl + 'legal-notice?' + query, {
         method: 'GET',
     })
+
+    if (!res.ok) {
+        throw new Error('Could not fetch legal notice: ' + res.status);
+    }
 
     const { data } = await res.json();
 
     return data?.attributes?.body;
 }
 
-export async function getPrivacyNotice() {
-    const res = await fetch(parsedStrapiUrl + 'privacy-notice', {
+export async function getPrivacyNotice(lang: string) {
+    const query = buildStrapiQuery({
+        locale: lang,
+    })
+    const res = await fetch(parsedStrapiUrl + 'privacy-notice?' + query, {
         method: 'GET',
     })
+
+    if (!res.ok) {
+        throw new Error('Could not fetch privacy notice: ' + res.status);
+    }
 
     const { data } = await res.json();
 
