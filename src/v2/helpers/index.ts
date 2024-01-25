6ff2709a16494c0d3a9970e5cfcf841188419be4
Andrei <andreigames9@gmail.com>
2024-01-25 22:57:44 +0100

    added export for parseStrapiDataToInterface

--- src/v2/helpers/index.ts
+++ src/v2/helpers/index.ts
@@ -1,3 +1,4 @@
 export { default as buildPageMetadata } from './buildPageMetadata';
 export { default as buildQsString } from './buildQsString';
 export { default as mapSections } from './mapSections';
+export { default as parseStrapiDataToInterface } from './parseStrapiData';
